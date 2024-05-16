b9b0dc53b1c840b3440c0082830a058300b9cd81
balckowl <shiggyjr1492@gmail.com>
2024-05-16 12:23:18 +0900

    fix: categorytitleが表示されないバグを修正しました。

--- src/app/(pages)/category/[slug]/page.tsx
+++ src/app/(pages)/category/[slug]/page.tsx
@@ -10,11 +10,11 @@ const CategoryDetail = async ({ params, searchParams }: { params: { slug: string
     const limit = 3; 
     const offset = (page - 1) * limit;
     const Sozaies = await getList({ filters: `category[contains]${slug}`, limit, offset })
-    const Category = await getCategoryList({ filters: `id[equals]${slug}`, limit, offset })
+    const Category = await getCategoryList({ filters: `id[equals]${slug}`})
 
     return (
         <div>
-            <SozaiList title={Category.contents[0].name} contents={Sozaies.contents} currentPage={page} totalCount={Sozaies.totalCount} limit={limit} />
+            <SozaiList title={Category.contents[0]?.name} contents={Sozaies.contents} currentPage={page} totalCount={Sozaies.totalCount} limit={limit} />
         </div>
     )
 }
