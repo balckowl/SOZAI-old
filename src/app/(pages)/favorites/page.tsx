33a98c53943b1a36c3b3bb5492eefe639e46a8e4
balckowl <shiggyjr1492@gmail.com>
2024-05-17 15:30:13 +0900

    fix: 一度favoriteをコメントアウトしました。

--- src/app/(pages)/favorites/page.tsx
+++ src/app/(pages)/favorites/page.tsx
@@ -1,44 +1,52 @@
-"use client"
-import { Suspense, useEffect, useState } from 'react';
-import SozaiList from "@/app/components/SozaiList/SozaiList";
-import { getList } from "@/libs/microcms";
-import { store } from "@/store/store";
-import { useSearchParams } from 'next/navigation';
+// "use client"
+// import { Suspense, useEffect, useState } from 'react';
+// import SozaiList from "@/app/components/SozaiList/SozaiList";
+// import { getList } from "@/libs/microcms";
+// import { store } from "@/store/store";
+// import { useSearchParams } from 'next/navigation';
 
-const Favorites = () => {
-    const [Sozaies, setSozaies] = useState<any>({});
-    const { likedIds } = store();
+// const Favorites = () => {
+//     const [Sozaies, setSozaies] = useState<any>({});
+//     const { likedIds } = store();
 
-    const searchParams = useSearchParams();
-    const page = searchParams.get('page') ? parseInt(searchParams.get('page') as string, 10) : 1;
-    const limit = 9;
-    const offset = (page - 1) * limit;
+//     const searchParams = useSearchParams();
+//     const page = searchParams.get('page') ? parseInt(searchParams.get('page') as string, 10) : 1;
+//     const limit = 9;
+//     const offset = (page - 1) * limit;
 
-    const getSozaiFavoriteData = async () => {
-        let data;
-        if (likedIds.length > 0) {
-            data = await getList({ 
-                filters: likedIds.map(id => `id[equals]${id}`).join('[or]'), 
-                limit, 
-                offset 
-            });
-        } else {
-            data = [];
-        }
-        setSozaies(data);
-    };
+//     const getSozaiFavoriteData = async () => {
+//         let data;
+//         if (likedIds.length > 0) {
+//             data = await getList({ 
+//                 filters: likedIds.map(id => `id[equals]${id}`).join('[or]'), 
+//                 limit, 
+//                 offset 
+//             });
+//         } else {
+//             data = [];
+//         }
+//         setSozaies(data);
+//     };
 
-    useEffect(() => {
-        getSozaiFavoriteData();
-    }, [likedIds, page]);
+//     useEffect(() => {
+//         getSozaiFavoriteData();
+//     }, [likedIds, page]);
 
-    return (
-        <div>
-            <Suspense fallback={<div>Loading...</div>}>
-                {Sozaies && <SozaiList title="お気に入り" contents={Sozaies.contents} />}
-            </Suspense>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <Suspense fallback={<div>Loading...</div>}>
+//                 {Sozaies && <SozaiList title="お気に入り" contents={Sozaies.contents} />}
+//             </Suspense>
+//         </div>
+//     );
+// };
 
-export default Favorites;
+// export default Favorites;
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
