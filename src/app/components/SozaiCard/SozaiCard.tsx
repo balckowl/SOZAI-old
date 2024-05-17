c0a2afbbf7c7c0f20f80e6af6809a4260a8c37ee
balckowl <shiggyjr1492@gmail.com>
2024-05-17 23:43:46 +0900

    remove: お気に入り機能を取り除きました。

--- src/app/components/SozaiCard/SozaiCard.tsx
+++ src/app/components/SozaiCard/SozaiCard.tsx
@@ -1,7 +1,6 @@
 "use client"
 import Image from "next/image"
 import Link from "next/link"
-import Star from "../Star/Star"
 import { store } from "@/store/store"
 
 const SozaiCard = ({ src, name, href, id }: { src: string, name: string, href: string, id: string }) => {
@@ -17,7 +16,6 @@ const SozaiCard = ({ src, name, href, id }: { src: string, name: string, href: s
             </Link>
             <div className="flex items-center justify-between">
                 <p className="text-[14px] md:text-[16px]">{name}</p>
-                <Star isLiked={likedIds.includes(id)} id={id} />
             </div>
         </div>
     )
